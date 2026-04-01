825d75dc5e626ae0d8e435bd7e998613da13e041
aleke <aleke.rysbek@mail.ru>
2026-04-01 22:13:25 +0500

    left

--- task/doctor-service/internal/model/doctor.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Doctor struct {
-	ID             string `json:"id"`
-	FullName       string `json:"full_name"`
-	Specialization string `json:"specialization"`
-	Email          string `json:"email"`
-}
